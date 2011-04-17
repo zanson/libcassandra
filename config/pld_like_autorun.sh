d009c08a0d855fc95ec68f46fc983ffce7ac10b3
Mateusz Korniak <mateusz@ant.gliwice.pl>
2011-04-17 21:38:45 +0200

    Really Makefiles work for me.

--- config/pld_like_autorun.sh
+++ config/pld_like_autorun.sh
@@ -9,11 +9,14 @@ automake -a -c -f --foreign
 
 ./configure LDFLAGS="-Wl,--as-needed -Wl,--no-copy-dt-needed-entries -Wl,-z,relro -Wl,-z,combreloc"  \
   CFLAGS="-O2 -fno-strict-aliasing -fwrapv -march=i686 -mtune=pentium4 -gdwarf-3 -g2"  \
-  CXXFLAGS="-O2 -fno-strict-aliasing -fwrapv -march=i686 -mtune=pentium4 -gdwarf-3 -g2  -Wno-error"   \
+  CXXFLAGS="-O2 -fno-strict-aliasing -fwrapv -march=i686 -mtune=pentium4 -gdwarf-3 -g2  -Wno-variadic-macros -Wno-deprecated"   \
   CPPFLAGS="-D_FORTIFY_SOURCE=2"   \
-  CC=i686-pld-linux-gcc   \
-  CXX=i686-pld-linux-g++  \
-  --host=i686-pld-linux --build=i686-pld-linux --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc  \
+  --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc  \
   --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib --libexecdir=/usr/lib --localstatedir=/var --sharedstatedir=/var/lib \
   --mandir=/usr/share/man --infodir=/usr/share/info --x-libraries=/usr/lib 
+
+# rm -r ~/tmp/libcassandra_install_test
+# mkdir ~/tmp/libcassandra_install_test
+
+
 
