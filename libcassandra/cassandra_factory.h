d26679d5cacff8ee49ca349d74e8eefaee11307f
Jeremiah Jordan <jeremiah.jordan@morningstar.com>
2011-07-19 11:17:37 -0500

    Make cassandra factory virtual for unit testing

--- libcassandra/cassandra_factory.h
+++ libcassandra/cassandra_factory.h
@@ -40,33 +40,33 @@ public:
   CassandraFactory(const std::string& server_list);
   CassandraFactory(const std::string& in_host, int in_port);
   CassandraFactory(const std::string& in_host, int in_port, int in_timeout);
-  ~CassandraFactory();
+  virtual ~CassandraFactory();
 
   /**
    * @return a shared ptr which points to a Cassandra client
    */
-  std::tr1::shared_ptr<Cassandra> create();
+  virtual std::tr1::shared_ptr<Cassandra> create();
 
   /**
    * @param[in] keyspace name of keyspace to associate this instance with
    * @return a shared ptr which points to a Cassandra client
    */
-  std::tr1::shared_ptr<Cassandra> create(const std::string& keyspace);
+  virtual std::tr1::shared_ptr<Cassandra> create(const std::string& keyspace);
 
   /**
    * @return port number associated with cassandra instances created
    */
-  int getPort() const;
+  virtual int getPort() const;
 
   /**
    * @return host name of cassandra instances created
    */
-  const std::string &getHost() const;
+  virtual const std::string &getHost() const;
 
   /**
    * @return URL of cassandra instances created
    */
-  const std::string &getURL() const;
+  virtual const std::string &getURL() const;
 
 private:
 
